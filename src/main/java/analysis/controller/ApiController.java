7174c55e843e4fd474fd93e37ce21782af467fa7
fingertipy <fingertipy@126.com>
2019-12-03 15:17:14 +0800

    configure call python util

--- src/main/java/analysis/controller/ApiController.java
+++ src/main/java/analysis/controller/ApiController.java
@@ -2,7 +2,9 @@ package analysis.controller;
 
 import analysis.entity.SampleEntity;
 import analysis.server.SampleService;
+import analysis.utils.PythonUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,12 +18,17 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class ApiController {
-
     @Autowired
     private SampleService sampleService;
 
     @GetMapping("/sample")
     public List<SampleEntity> sample(){
         return sampleService.selectAllSample();
+    }
+
+    @GetMapping("/exec")
+    public void exec(){
+        String pypath = "D:\\repository\\analysis_python\\analysis\\model.py";
+        PythonUtils.exec(pypath);
     }
 }
---
+++ src/main/java/analysis/utils/PythonUtils.java
@@ -0,0 +1,60 @@
+package analysis.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * @Description python utils
+ * @Author      dayu
+ * @Date        2019/12/3 10:54
+ * @Version     v1.0
+ */
+@Component
+public class PythonUtils {
+
+    private PythonUtils(){}
+
+    //python executor
+    private static String executor;
+
+    @Value("${python.executor}")
+    public void setExecutor(String executor){
+        this.executor = executor;
+    }
+
+    //logger
+    private static Logger logger = LoggerFactory.getLogger(PythonUtils.class);
+
+    /**
+     * @Description execute python program
+     * @Author      dayu
+     * @Date        2019/12/3 10:56
+     * @Param
+     * @Return      void
+     */
+    public static void exec(String path){
+        try {
+            logger.info("======================== Execute python program : start===========================");
+            //执行脚本
+            Process process = Runtime.getRuntime().exec(new String[]{executor, path});
+            //接口执行结果
+            BufferedReader buffer = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line = null;
+            //处理执行结果
+            while ((line = buffer.readLine()) != null){
+                System.out.println(line);
+            }
+            //关闭资源
+            buffer.close();
+            process.waitFor();
+            logger.info("======================== Execute python program :   end===========================");
+        } catch (Exception e) {
+            logger.error("Execute python program exception: ", e);
+        }
+    }
+}
